90da9f0a3618fe5db6d67fed9c5bb9da74bfbc5a
ikebf <89275242+ikebf@users.noreply.github.com>
2022-10-25 12:53:01 -0400

    More path fixes

--- Looking_at_Data/initLesson.R
+++ Looking_at_Data/initLesson.R
@@ -9,7 +9,7 @@ swirl_options(swirl_logging = TRUE)
 
 # Path to data
 .datapath <- file.path(.get_course_path(),
-                      'R_Programming', 'Looking_at_Data',
+                      '377_swirl', 'Looking_at_Data',
                       'plant-data.txt')
 # Read in data
 plants <- read.csv(.datapath, strip.white=TRUE, na.strings="")
--- vapply_and_tapply/initLesson.R
+++ vapply_and_tapply/initLesson.R
@@ -9,7 +9,7 @@ swirl_options(swirl_logging = TRUE)
 
 
 # Path to installed lesson
-.lessonpath <- file.path(.get_course_path(), "R_Programming",
+.lessonpath <- file.path(.get_course_path(), "377_swirl",
                          "lapply_and_sapply")
 # Path to dataset
 .datapath <- file.path(.lessonpath, "flag.data.txt")
